0e89be4027913c534a0027877ea974c03dabf0fd
chethan <chethan@chethan-ThinkPad>
2013-11-03 23:06:55 +0530

    rebuilding

---
+++ contact/src/com/chethan/utils/SearchUtil.java
@@ -0,0 +1,18 @@
+package com.chethan.utils;
+
+import java.util.ArrayList;
+
+import com.chethan.objects.SimpleContact;
+
+public class SearchUtil {
+
+	public static ArrayList<SimpleContact> search(ArrayList<SimpleContact> contactList, String string) {
+		return null;
+	}
+
+	public static ArrayList<SimpleContact> SortByPriority(ArrayList<SimpleContact> searchList,
+			ArrayList<SimpleContact> callHistorySimpleContactList) {
+		return null;
+	}
+
+}
